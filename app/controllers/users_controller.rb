f04a310eb266b7c669dd3cd7c42b1cc1b9f7b608
github username <metelskyi88@gmail.com>
2019-02-05 15:50:24 -0500

    CRUD for user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,22 +7,28 @@ class UsersController < ApplicationController
   end
 
   def show
-
   end
 
   def new
+    @user = User.new
   end
 
   def create
+    @user = User.create(user_params)
+    redirect_to @user
   end
 
   def edit
   end
 
   def update
+    @user.update(user_params)
+    redirect_to @user
   end
 
   def destroy
+    @user.destroy
+    redirect_to users_path
   end
 
   private
